4db402734e3b7627823b92e778f7b47097c3b5ec
BalajiM012 <113889348+BalajiM012@users.noreply.github.com>
2026-01-03 21:57:17 +0530

    Refactor offline song management functions

--- frontend/src/utils/offlineCache.js
+++ frontend/src/utils/offlineCache.js
@@ -12,20 +12,23 @@ export const getDB = () =>
     }
   });
 
-// Save uploaded song
-export const saveSongOffline = async (songId, file) => {
+// song = { id, title, artist, album, cover, file }
+export const saveSongOffline = async (song) => {
   const db = await getDB();
-  await db.put(STORE_NAME, file, songId);
+  await db.put(STORE_NAME, song, song.id);
 };
 
-// Get song for playback
-export const getSongOffline = async (songId) => {
+export const getSongOffline = async (id) => {
   const db = await getDB();
-  return db.get(STORE_NAME, songId);
+  return db.get(STORE_NAME, id);
 };
 
-// List all offline songs
-export const getAllOfflineSongs = async () => {
+export const getAllSongsOffline = async () => {
   const db = await getDB();
-  return db.getAllKeys(STORE_NAME);
+  return db.getAll(STORE_NAME);
+};
+
+export const deleteSongOffline = async (id) => {
+  const db = await getDB();
+  await db.delete(STORE_NAME, id);
 };
